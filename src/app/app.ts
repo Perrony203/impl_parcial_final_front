5f6c8c711496ec71129563212638c4a4b157bdb3
PyroSoul56 <tomi.que.sa@gmail.com>
2025-10-18 17:22:28 -0500

    Creada la base

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class App {
+  readonly title = signal<string>('impl_parcial_final_front');
+}
